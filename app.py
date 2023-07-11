3cc0e0296cde77fb9145926bb448013e159feadc
delossantos <yeury-msr@hotmail.com>
2023-07-11 22:22:34 +0100

    Initial commit: Set up project structure, created the HTML files for web pages, built the registration/login logic and started to build the wallet logic

--- app.py
+++ app.py
@@ -1,118 +1,215 @@
-from flask import Flask, render_template, request, jsonify
-from flask_jwt_extended import JWTManager, create_access_token, jwt_required, get_jwt_identity
-import pymysql
-from werkzeug.security import generate_password_hash, check_password_hash
-from dotenv import load_dotenv
-from config.config import get_config
-import logging
-import os
+# Import required libraries
+from flask import Flask, render_template, request, jsonify, session
+from flask import redirect, url_for
+import jwt
+from datetime import datetime, timedelta
+import mysql.connector
+import bcrypt
 
-load_dotenv()  # Load environment variables from .env file
-
+# Create a Flask app
 app = Flask(__name__)
-app.config.from_object(get_config())
+app.config['SECRET_KEY'] = 'cct'
 
-# Set the logging level to capture all messages
-logging.basicConfig(level=logging.DEBUG)
+# MySQL configuration
+db = mysql.connector.connect(
+    host="localhost",
+    user="cct",
+    password="cctcollege2023*",
+    database="walletwatch_db"
+)
+cursor = db.cursor()
 
-# Create a logger instance for your application
-logger = app.logger
-
-try:
-    mysql = pymysql.connect(
-        host=app.config['MYSQL_HOST'],
-        port=app.config['MYSQL_PORT'],
-        user=app.config['MYSQL_USER'],
-        password=app.config['MYSQL_PASSWORD'],
-        db=app.config['MYSQL_DB'],
-        charset='utf8mb4',
-        cursorclass=pymysql.cursors.DictCursor
-    )
-    logger.info("Database connection established")
-
-    # Read and execute the SQL file to create tables
-    with app.open_resource('sql/create_tables.sql', mode='r') as f:
-        with mysql.cursor() as cursor:
-            cursor.execute(f.read())
-
-    jwt = JWTManager(app)
-
-except Exception as e:
-    logger.error("An error occurred during database connection: %s", str(e))
-
-# User model
-class User:
-    def __init__(self, id, email, password):
-        self.id = id
-        self.email = email
-        self.password = password
-
-# Database queries
-def get_user(email):
-    cursor = mysql.cursor()
-    cursor.execute("SELECT * FROM users WHERE email = %s", (email,))
-    user_data = cursor.fetchone()
-    cursor.close()
-    if user_data:
-        return User(user_data['id'], user_data['email'], user_data['password'])
+# Function to get logged-in user's ID
+def get_logged_in_user_id():
+    token = session.get('token')
+    if token:
+        try:
+            decoded_token = jwt.decode(token, app.config['SECRET_KEY'], algorithms=['HS256'])
+            return decoded_token.get('user_id')
+        except jwt.ExpiredSignatureError:
+            # Token has expired
+            return None
     return None
 
-def create_user(email, password):
-    cursor = mysql.cursor()
-    cursor.execute("INSERT INTO users (email, password) VALUES (%s, %s)", (email, generate_password_hash(password)))
-    mysql.commit()
-    cursor.close()
-
-@app.route('/')
-def index():
-    return render_template('login.html')
-
-@app.route('/login', methods=['GET', 'POST'])
-def login():
-    if request.method == 'POST':
-        email = request.form['email']
-        password = request.form['password']
-        user = get_user(email)
-        if user and check_password_hash(user.password, password):
-            access_token = create_access_token(identity=user.id)
-            return jsonify(access_token=access_token), 200
-        return jsonify({'msg': 'Invalid email or password'}), 401
-    return render_template('login.html')
-
+# User Registration route
 @app.route('/signup', methods=['GET', 'POST'])
 def signup():
     if request.method == 'POST':
         email = request.form['email']
         password = request.form['password']
-        if get_user(email):
-            return jsonify({'msg': 'User already exists'}), 400
-        create_user(email, password)
-        return jsonify({'msg': 'User created successfully'}), 201
+
+        # Check if the email is already taken
+        query = "SELECT * FROM users WHERE email = %s"
+        cursor.execute(query, (email,))
+        if cursor.fetchone():
+            return jsonify({'message': 'Email already exists'}), 409
+        
+        # Hash the password
+        hashed_password = bcrypt.hashpw(password.encode('utf-8'), bcrypt.gensalt())
+
+        # Insert the new user into the users table
+        query = "INSERT INTO users (email, password) VALUES (%s, %s)"
+        cursor.execute(query, (email, hashed_password))
+        db.commit()
+
+        return jsonify({'message': 'User registered successfully'})
+
     return render_template('signup.html')
 
+# User Login route
+@app.route('/login', methods=['GET', 'POST'])
+def login():
+    if request.method == 'POST':
+        email = request.form['email']
+        password = request.form['password']
+
+        # Validate the email
+        query = "SELECT * FROM users WHERE email = %s"
+        cursor.execute(query, (email,))
+        user = cursor.fetchone()
+
+        # If user exist within the database
+        if user:
+            stored_password = user[2]
+
+            # Verify password using bcrypt
+            if bcrypt.checkpw(password.encode('utf-8'), stored_password.encode('utf-8')):
+                # Password matches
+
+                # Get the user_id
+                user_id = user[0]
+                
+                # Generate JWT token
+                token = jwt.encode({'user_id': user_id, 'exp': datetime.utcnow() + timedelta(minutes=30)}, app.config['SECRET_KEY'])
+                session['token'] = token
+
+                return jsonify({'token': token}), 200
+                         
+            return jsonify({'message': 'Invalid email or password'}), 401
+        
+        # User does not exist
+        return jsonify({'message': 'User does not exist'}), 401
+
+    return render_template('login.html')
+
+# Dashboard route
 @app.route('/dashboard')
-@jwt_required()
 def dashboard():
-    current_user = get_jwt_identity()
-    return render_template('dashboard.html', user=current_user)
+    # Add your code for the dashboard functionality here
+    return render_template('dashboard.html')
 
+# Expenses route
 @app.route('/expenses')
-@jwt_required()
 def expenses():
-    current_user = get_jwt_identity()
-    return render_template('expenses.html', user=current_user)
+    # Add your code for the expenses functionality here
+    return render_template('expenses.html')
 
+# Wallets route
+# Wallets route
+@app.route('/wallets', methods=['GET', 'POST'])
+def wallets():
+    if request.method == 'POST':
+        wallet_name = request.form['walletName']
+        user_id = get_logged_in_user_id()
+
+        if user_id is not None:
+            # Check if the user_id exists in the users table
+            query = "SELECT * FROM users WHERE user_id = %s"
+            cursor.execute(query, (user_id,))
+            user = cursor.fetchone()
+
+            if user:
+                # Insert the new wallet into the wallets table
+                query = "INSERT INTO wallets (name, status, user_id) VALUES (%s, 'Active', %s)"
+                cursor.execute(query, (wallet_name, user_id))
+                db.commit()
+
+                return jsonify({'message': 'Wallet added successfully'})
+            else:
+                return jsonify({'message': 'User does not exist'}), 401
+        else:
+            return jsonify({'message': 'Invalid user'}), 401
+
+    user_id = get_logged_in_user_id()
+
+    if user_id is not None:
+        wallets = get_wallets()  # Fetch and sort the wallets
+
+        return render_template('wallets.html', wallets=wallets)
+    else:
+        return jsonify({'message': 'Invalid user'}), 401
+
+# Sort wallets to display newer wallet next to the add new wallet button
+def get_wallets():
+    user_id = get_logged_in_user_id()
+
+    if user_id is not None:
+        # Fetch the wallets for the logged-in user from the database
+        #query = "SELECT * FROM wallets WHERE user_id = %s OR user_id IN (SELECT wallet_id FROM wallets_users WHERE user_id = %s) ORDER BY created_at DESC"
+
+        # Fetch the wallets for the logged-in user and shared wallets from the database
+        query = """
+        SELECT DISTINCT w.* FROM wallets AS w LEFT JOIN wallets_users AS u ON w.wallet_id = u.wallet_id
+        WHERE w.user_id = %s OR u.user_id = %s
+        ORDER BY w.created_at DESC
+        """
+        cursor.execute(query, (user_id,user_id))
+        wallets = cursor.fetchall()
+
+        return wallets
+    else:
+        return []  # Return an empty list if the user is not logged in
+
+
+@app.route('/wallets/<string:wallet_name>', methods=['GET', 'POST'])
+def wallet_details(wallet_name):
+    wallet = get_wallet_by_name(wallet_name)
+    if wallet:
+        return render_template('wallet_details.html', wallet=wallet)
+    else:
+        return jsonify({'message': 'Wallet not found'}), 404
+
+def get_wallet_by_name(wallet_name):
+    user_id = get_logged_in_user_id()
+
+    if user_id is not None:
+        # Fetch the wallet details from the database based on the wallet name and user_id
+        query = "SELECT * FROM wallets WHERE name = %s AND user_id = %s"
+        cursor.execute(query, (wallet_name, user_id))
+        wallet = cursor.fetchone()
+
+        if wallet:
+            # If the wallet exists, return it as a dictionary
+            wallet_dict = {
+                'id': wallet[0],
+                'name': wallet[1],
+                'status': wallet[2],
+                'user_id': wallet[3],
+                'created_at': wallet[4]
+            }
+            return wallet_dict
+
+    # If the wallet doesn't exist or the user is not logged in, return None
+    return None
+
+# Analytics route
 @app.route('/analytics')
-@jwt_required()
 def analytics():
-    current_user = get_jwt_identity()
-    return render_template('analytics.html', user=current_user)
+    # Add your code for the analytics functionality here
+    return render_template('analytics.html')
 
+# Settings route
 @app.route('/settings')
-@jwt_required()
 def settings():
-    current_user = get_jwt_identity()
-    return render_template('settings.html', user=current_user)
+    # Add your code for the settings functionality here
+    return render_template('settings.html')     
 
+# Logout route
+@app.route('/logout')
+def logout():
+    session.pop('token', None)
+    return redirect(url_for('login'))  
+
+# Run the app
 if __name__ == '__main__':
-    app.run(host='0.0.0.0', debug=True)
+    app.run()
--- sql/create_tables.sql
+++ sql/create_tables.sql
@@ -1,23 +1,54 @@
 -- Create the users table
 CREATE TABLE IF NOT EXISTS users (
-    id INT AUTO_INCREMENT PRIMARY KEY,
-    email VARCHAR(255) NOT NULL,
+    user_id INT AUTO_INCREMENT PRIMARY KEY,
+    email VARCHAR(100) NOT NULL,
     password VARCHAR(255) NOT NULL
 );
 
+-- Create the wallets table
+CREATE TABLE IF NOT EXISTS wallets (
+    wallet_id INT AUTO_INCREMENT PRIMARY KEY,
+    name VARCHAR(50) NOT NULL,
+    status VARCHAR(15)
+);
+
+-- Create the types table
+CREATE TABLE IF NOT EXISTS types (
+    type_id INT AUTO_INCREMENT PRIMARY KEY,
+    name VARCHAR(15) NOT NULL
+);
+
+-- Insert the pre-defined types
+INSERT INTO types (name) VALUES ('Basic'), ('Personal'), ('Saving');
+
 -- Create the categories table
 CREATE TABLE IF NOT EXISTS categories (
-    id INT AUTO_INCREMENT PRIMARY KEY,
-    name VARCHAR(255) NOT NULL
+    category_id INT AUTO_INCREMENT PRIMARY KEY,
+    name VARCHAR(50) NOT NULL
 );
 
 -- Create the expenses table
 CREATE TABLE IF NOT EXISTS expenses (
-    id INT AUTO_INCREMENT PRIMARY KEY,
-    amount DECIMAL(10, 2) NOT NULL,
-    description TEXT,
-    category_id INT,
+    expense_id INT AUTO_INCREMENT PRIMARY KEY,
     user_id INT,
-    FOREIGN KEY (category_id) REFERENCES categories(id),
-    FOREIGN KEY (user_id) REFERENCES users(id)
+    category_id INT,
+    name VARCHAR(50),
+    amount DECIMAL(10, 2) NOT NULL,
+    type_id INT,
+    description VARCHAR(180),
+    place VARCHAR(50),
+    wallet_id INT,
+    FOREIGN KEY (category_id) REFERENCES categories(category_id),
+    FOREIGN KEY (user_id) REFERENCES users(user_id),
+    FOREIGN KEY (type_id) REFERENCES types(type_id),
+    FOREIGN KEY (wallet_id) REFERENCES wallets(wallet_id)
+);
+
+-- Create the wallets_users table (to represent the many-to-many relationship between users and wallets)
+CREATE TABLE IF NOT EXISTS wallets_users (
+    wallet_id INT,
+    user_id INT,
+    PRIMARY KEY (wallet_id, user_id),
+    FOREIGN KEY (wallet_id) REFERENCES wallets(wallet_id),
+    FOREIGN KEY (user_id) REFERENCES users(user_id)
 );
